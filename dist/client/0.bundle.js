f23d306b51cd38d7b38caf151a7cd734dab96fd8
Bradley Few <few.bradley@gmail.com>
2018-05-04 16:13:37 +0200

    moves clean script above bundle scripts. commits /dist to confirm output.

--- dist/client/0.bundle.js
+++ dist/client/0.bundle.js
@@ -1,15 +1,1 @@
-(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[0],{
-
-/***/ "./client/Bar.js":
-/*!***********************!*\
-  !*** ./client/Bar.js ***!
-  \***********************/
-/*! exports provided: default */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ \"./node_modules/react/index.js\");\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);\n\n\nconst Bar = () => {\n  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(\n    \"div\",\n    null,\n    react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(\n      \"div\",\n      { onClick: e => {\n          console.log(e);\n        }\n      },\n      \"WAT\"\n    ),\n    react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(\n      \"div\",\n      null,\n      \"barrbabababababaab\"\n    ),\n    react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(\n      \"div\",\n      null,\n      \"barrbabababababaab\"\n    ),\n    react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(\n      \"div\",\n      null,\n      \"barrbabababababaab\"\n    )\n  );\n};\n\n/* harmony default export */ __webpack_exports__[\"default\"] = (Bar);\n\n//# sourceURL=webpack:///./client/Bar.js?");
-
-/***/ })
-
-}]);
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{8:function(o,f,n){"use strict";n.r(f);var e=n(0),d=n.n(e);f.default=(()=>d.a.createElement("div",null,"Foofoofoofofofofdddofo"))}}]);
--- dist/client/1.bundle.js
+++ dist/client/1.bundle.js
@@ -1,15 +1,1 @@
-(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[1],{
-
-/***/ "./client/Foo.js":
-/*!***********************!*\
-  !*** ./client/Foo.js ***!
-  \***********************/
-/*! exports provided: default */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ \"./node_modules/react/index.js\");\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);\n\n\nconst Foo = () => {\n  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(\n    \"div\",\n    null,\n    \"Foofoofoofofofofofo\"\n  );\n};\n\n/* harmony default export */ __webpack_exports__[\"default\"] = (Foo);\n\n//# sourceURL=webpack:///./client/Foo.js?");
-
-/***/ })
-
-}]);
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{9:function(a,b,e){"use strict";e.r(b);var l=e(0),n=e.n(l);b.default=(()=>n.a.createElement("div",null,n.a.createElement("div",{onClick:a=>{console.log(a)}},"bradley"),n.a.createElement("div",null,"barrbabababababaab"),n.a.createElement("div",null,"barrbababadddbababaab"),n.a.createElement("div",null,"barrbabababababaab")))}}]);
